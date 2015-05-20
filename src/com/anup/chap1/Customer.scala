8be943ec9c470a6742a9620267e02191d25674ac
Anup Nair <anup@sumologic.com>
2015-05-20 14:44:06 +0530

    move frequentRenterPoints calculation to a private function

--- src/com/anup/chap1/Customer.scala
+++ src/com/anup/chap1/Customer.scala
@@ -5,23 +5,24 @@ package com.anup.chap1
  */
 class Customer(_name:String, _rentals:Vector[Rental]) {
   def statements:String = {
-    var frequentRenterPoints:Int = 0
     var result:String = s"Rental Record for ${_name} \n"
     _rentals.foreach(
       rental => {
-        frequentRenterPoints += rental.getFrequentRenterPoints()
-
         // show results
         result += s"\t ${rental._movie._title}\t${rental.getCharge()}"
       }
     )
 
     result += s"Amount owed is ${getTotalCharge()} \n"
-    result += s"You earned ${frequentRenterPoints} frequent renter points\n"
+    result += s"You earned ${getTotalFrequentRenterPoints()} frequent renter points\n"
     result
   }
 
   private def getTotalCharge() : Double = {
     _rentals.map(_.getCharge()).sum
+  }
+
+  private def getTotalFrequentRenterPoints() : Int = {
+    _rentals.map(_.getFrequentRenterPoints()).sum
   }
 }
