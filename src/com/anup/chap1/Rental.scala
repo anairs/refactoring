aca962fdf405362a351be1a283271276626fc7ab
Anup Nair <anup@sumologic.com>
2015-05-20 14:25:56 +0530

    move amountFor to Rental class

--- src/com/anup/chap1/Rental.scala
+++ src/com/anup/chap1/Rental.scala
@@ -4,5 +4,24 @@ package com.anup.chap1
  * Created by anup on 20/05/15.
  */
 class Rental(val _movie:Movie, val _daysRented:Int) {
+  def getCharge(): Double = {
+    var thisAmount: Double = 0
+    rental._movie._priceCode match {
+      case Movie.REGULAR =>
+        thisAmount += 2
+        if (rental._daysRented > 2)
+          thisAmount += (rental._daysRented - 2) * 1.5
+
+      case Movie.NEW_RELEASE =>
+        thisAmount += 2
+        thisAmount += rental._daysRented * 3
+
+      case Movie.CHILDRENS =>
+        thisAmount += 2
+        if (rental._daysRented > 3)
+          thisAmount += (rental._daysRented - 32) * 1.5
+    }
+    thisAmount
+  }
 
 }
